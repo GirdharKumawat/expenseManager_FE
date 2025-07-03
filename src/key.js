e7b08e928c0c40b0e0810e4d22de944a2b15666f
Girdhar Kumawat <girdharkumawat3108@gmail.com>
2025-07-03 13:51:34 +0530

    feat: Update API endpoint to use the production URL

--- src/key.js
+++ src/key.js
@@ -1,3 +1,3 @@
 // const API_ENDPOINT ='http://127.0.0.1:8000/';
-const API_ENDPOINT ='http://192.168.29.89:8000/';
+const API_ENDPOINT ='https://api.expensemanager.girdharkumawat.site/';
 export default API_ENDPOINT ;
